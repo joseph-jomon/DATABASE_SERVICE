449ea4836cb6bdbab77d59a7bd273c56bf0d0ff7
joseph-jomon <josephjomon401@gmail.com>
2025-03-03 19:00:07 +0100

    Update entrypoint script to copy Elasticsearch password secret and set permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-# Set the correct permissions for the secret file
-chmod 600 /run/secrets/elastic_password
+# Copy the secret file to a writable location
+cp /run/secrets/elastic_password /usr/share/elasticsearch/config/elastic_password
+
+# Set the correct permissions for the copied secret file
+chmod 600 /usr/share/elasticsearch/config/elastic_password
+
+# Update the environment variable to point to the new location
+export ELASTIC_PASSWORD_FILE=/usr/share/elasticsearch/config/elastic_password
 
 # Execute the original entrypoint
 exec /usr/local/bin/docker-entrypoint.sh "$@"
