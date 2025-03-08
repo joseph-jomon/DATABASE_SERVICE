a113ca41c635f302e2a3e851ec8dd73a1b5603ce
joseph-jomon <josephjomon401@gmail.com>
2025-03-08 10:33:23 +0100

    changed to basic

--- vdb_app/services/vdb_es_client.py
+++ vdb_app/services/vdb_es_client.py
@@ -18,9 +18,9 @@ class VDBConnection:
     def __init__(self, host: str, timeout: int):
         self.client = AsyncElasticsearch(
         #hosts=[{"host": host, "port": 9200, "scheme": "http"}],
-       hosts=[{"host": host, "port": 9200, "scheme": "https"}],
-       basic_auth=(es_user,es_password),
-        ssl_context=ssl.create_default_context(cafile=cert_path),
+       hosts=[{"host": host, "port": 9200, "scheme": "http"}],
+       #basic_auth=(es_user,es_password),
+       # ssl_context=ssl.create_default_context(cafile=cert_path),
         timeout=timeout)
 
     async def ping(self):
