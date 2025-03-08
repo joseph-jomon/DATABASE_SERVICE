64f5149e4f2f8ee8e9941b9b5c946769f2b5047f
joseph-jomon <josephjomon401@gmail.com>
2025-03-08 09:54:32 +0100

    changed some config vals

--- vdb_app/vdb_config.py
+++ vdb_app/vdb_config.py
@@ -1,7 +1,7 @@
 # vdb_app/vdb_config.py
 
 class VDBSettings:
-    ELASTICSEARCH_HOST: str = "https://elasticsearch:9200" # see how_to_name_host.md
+    ELASTICSEARCH_HOST: str = "elasticsearch" # see how_to_name_host.md
     ELASTICSEARCH_INDEX: str = "immo"  # Default index name
     TIMEOUT: int = 90
 
